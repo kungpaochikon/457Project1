abc02383097d9324ccb547aaf08746671a2c6e25
gary <flemingg@mail.gvsu.edu>
2017-10-09 11:39:39 -0400

    no idea

---
+++ src/cis457Project1/Test.java
@@ -0,0 +1,13 @@
+package cis457Project1;
+
+public class Test {
+	    public static void main(String[] args) {
+	    	int squared_2 = square(2);//This should be 4;
+	    	int squared_4 = square(4);//This should be 16;
+	    }
+	    public static int square(int x){
+	    	int squared_value = x * x;
+	    	return squared_value;
+	    }
+
+}
